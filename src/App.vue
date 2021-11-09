5edec0ec8542f42ccd1aedcf74610143561c8ac7
Winder <iqwijodiro@gmail.com>
2021-11-08 21:38:07 -0400

    Tutorial app: Resources list

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+     <the-header title="RememberMe" />
+    <the-resources></the-resources>
+    </div>
+</template>
+<script>
+import TheHeader from './components/layouts/TheHeader.vue';
+import TheResources from './components/learning-resources/TheResources.vue';
+export default {
+    components: { TheHeader, TheResources },
+}
+</script>
+<style>
+     @import url('https://fonts.googleapis.com/css2?family=Prompt:wght@300;400;700&display=swap');
+
+     * {
+         box-sizing: border-box;
+     }
+
+     html {
+         font-family: 'Prompt', sans-serif;
+     }
+     body {
+         margin: 0;
+     }
+</style>
